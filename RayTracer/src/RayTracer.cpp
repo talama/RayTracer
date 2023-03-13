81e7e1ea89a98bbe7ebf4c630dd593811cc866e3
Talama <talama@proton.me>
2023-03-13 23:22:04 +0100

    feat(Raytracer): Implemented Renderer class

--- RayTracer/src/RayTracer.cpp
+++ RayTracer/src/RayTracer.cpp
@@ -1,9 +1,8 @@
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
-
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
+#include "Renderer.h"
 
 using namespace Walnut;
 
@@ -22,10 +21,14 @@ public:
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 		ImGui::Begin("Viewport");
+
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
-		if(m_Image)
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+
+		auto image = m_Renderer.GetFinalImage();
+
+		if(image)
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() });
 		
 		ImGui::End();
 		ImGui::PopStyleVar();
@@ -36,25 +39,14 @@ public:
 	{
 		Timer timer;
 
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
-		{
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
+		m_Renderer.onResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
 		
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
-		{
-			m_ImageData[i] = Random::UInt();
-			m_ImageData[i] |= 0xff000000;
-		}
-		m_Image->SetData(m_ImageData);
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	std::shared_ptr<Image> m_Image;
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
-	uint32_t* m_ImageData = nullptr;
 	float m_LastRenderTime = 0;
 };
 
